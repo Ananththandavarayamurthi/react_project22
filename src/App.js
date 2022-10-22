f2ad18dfe6aeba3ab0a405613b2ce962550d60e0
Ananththandavarayamurthi <ananthcivil9876@gmail.com>
2022-10-23 02:49:58 +0530

    f

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+import './App.css';
+import Home from './components/Home';
+
+
+
+function App() {
+  return (
+        <>
+        
+        <Home/>
+        
+        </>
+  );
+}
+
+export default App;
